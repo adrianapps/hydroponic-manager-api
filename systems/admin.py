202670514848d82acbbfd8174298d6478e1db7ed
tempestpulse <bulsar@tutanota.com>
2024-06-06 00:55:12 +0200

    cutomize django admin

--- systems/admin.py
+++ systems/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 
 from .models import HydroponicSystem, Measurement
 
-admin.site.register(HydroponicSystem)
-admin.site.register(Measurement)
+
+class MeasurementInLine(admin.TabularInline):
+    model = Measurement
+    extra = 3
+
+
+class HydroponicSystemAdmin(admin.ModelAdmin):
+    inlines = [MeasurementInLine]
+    list_display = ['name', 'owner', 'slug']
+    list_filter = ['owner']
+    search_fields = ['name', 'owner__username', 'slug']
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ['system', 'timestamp']
+    list_filter = ['system', 'timestamp']
+    search_fields = ['system__name', 'timestamp', 'temperature', 'ph', 'tds']
+
+
+admin.site.register(HydroponicSystem, HydroponicSystemAdmin)
+admin.site.register(Measurement, MeasurementAdmin)
